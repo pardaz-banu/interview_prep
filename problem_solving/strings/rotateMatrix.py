484f7d952ec395cc8a04119648e6f8db7a4bb59c
pardaz-banu <45086957+pardaz-banu@users.noreply.github.com>
2023-09-11 12:24:52 -0500

    Update rotateMatrix.py

--- problem_solving/strings/rotateMatrix.py
+++ problem_solving/strings/rotateMatrix.py
@@ -3,15 +3,15 @@ First swap the first half matrix diagnal side keeping diagonal constant
 and then swap row by half
 '''
 
-ip = [[1,2,3],[4,5,6],[7,8,9]]
-for i in range(len(ip[0])):
-    for j in range(i,len(ip[0])):
-        ip[i][j], ip[j][i] = ip[j][i], ip[i][j]
-m = (len(ip[0])-1)//2
-t = len(ip[0])
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+for i in range(len(matrix[0])):
+    for j in range(i,len(matrix[0])):
+        matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+m = (len(matrix[0])-1)//2
+t = len(matrix[0])
 #print(ip)
-for col in range(len(ip)):
+for col in range(len(matrix)):
     for row in range(m):
-        ip[row][col], ip[t-row-1][col] = ip[t-row-1][col], ip[row][col]
-    print(ip)
-print(ip)
+        matrix[row][col], matrix[t-row-1][col] = matrix[t-row-1][col], matrix[row][col]
+    print(matrix)
+print(matrix)
