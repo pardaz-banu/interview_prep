f12b136f53a320aab05f0325f59b4dda35d91a6b
pardaz-banu <45086957+pardaz-banu@users.noreply.github.com>
2023-10-11 23:06:05 -0400

    Create mergeAlternatively.py

---
+++ problem_solving/leetcode/mergeAlternatively.py
@@ -0,0 +1,39 @@
+'''You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+
+ 
+
+Example 1:
+
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+Example 2:
+
+Input: word1 = "ab", word2 = "pqrs"
+Output: "apbqrs"
+Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+word1:  a   b 
+word2:    p   q   r   s
+merged: a p b q   r   s
+'''
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        i, j = 0,0
+        res = ""
+        while i < len(word1) and j < len(word2):
+            res += word1[i] + word2[j]
+            i += 1
+            j += 1
+        while i < len(word1):
+            res += word1[i]
+            i += 1
+        while j < len(word2):
+            res += word2[j]
+            j += 1
+        return res
+        
